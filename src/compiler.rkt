15dad52766ba74c52e3b71c3d37a875a90181662
Philip Dumaresq <phdumaresq@protonmail.com>
2020-11-19 20:23:55 -0500

    Rewrote the compiler file as a pipeline of composing functions so that each phase can be called individually

--- src/compiler.rkt
+++ src/compiler.rkt
@@ -1,22 +1,33 @@
 (module click/compliler racket/base
+  (require racket/function)
   (require threading)
   (require
    "./frontend/lexer.rkt"
    "./frontend/parser.rkt"
    "./frontend/expander.rkt"
+   "./frontend/check-syntax.rkt"
    "./frontend/typechecker.rkt"
    )
 
-  (provide compile-click)
+  (provide
+   compile/tokenize
+   compile/parse
+   compile/check-syntax
+   compile/macro-expand
+   compile/typecheck
+   compile/click)
 
-  (define [compile-click input]
-    (~> input
-        (run-front-end-toolchain)))
+  (define compile/tokenize tokenize)
+  (define compile/parse (compose1 parse compile/tokenize))
+  (define compile/macro-expand (compose1 macro-expand compile/parse))
+  (define compile/check-syntax (compose1 check-syntax compile/macro-expand))
+  (define compile/typecheck (compose1 typecheck compile/check-syntax))
 
-  (define [run-front-end-toolchain input]
-    (~> input
-        (tokenize)
-        (parse)
-        (expander)))
+  (define compile/frontend-toolchain compile/typecheck)
+  (define compile/backend-toolchain identity)
+
+  (define compile/click
+    (compose1 compile/frontend-toolchain
+              compile/backend-toolchain))
 
   )
