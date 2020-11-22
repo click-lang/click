19459dea299437b7156cb16bdff0de4b2de56746
Philip Dumaresq <phdumaresq@protonmail.com>
2020-11-21 21:47:04 -0500

    Got pattern matching functions to expand properly into pattern matching in expansion

--- src/frontend/expander.rkt
+++ src/frontend/expander.rkt
@@ -21,8 +21,64 @@
        (cons (list (Token a b 'reader d) x) (macro-expand xs))]
       [(cons ':sb xs) (cons ':sb xs)]
       [(cons ':cb xs) (cons ':cb xs)]
+      [(cons (Token a b c "let") xs)
+       (expand-let (cons (Token a b c "let") xs))]
       [(cons (Token a b c d) xs)
        (cons (Token a b c d) (macro-expand xs))]
       [(cons (list exprs ...) xs)
        (cons (macro-expand exprs) (macro-expand xs))]
-      [(list) (list)])))
+      [(list) (list)]))
+
+  (: expand-cond (-> Ast Ast))
+  (define [expand-cond stx] stx)
+
+  (: expand-let (-> Ast Ast))
+  (define [expand-let stx]
+    (match stx
+      ;; Expand a typed `let`
+      [(list l (Token a b 'symbol id) (Token c d e ":") type value)
+       (list (list (Token c d e ":") (Token a b 'symbol id) type)
+             (list l (Token a b 'symbol id) value))]
+      ;; Expand function definition
+      ;; (let f [x] (+ x 2)) -> (let f (fn [x] (+ x 2)))
+      [(list l (Token a b 'symbol id) (cons ':sb ids) body ...)
+       (list l (Token a b 'symbol id)
+             (cons (Token a b 'symbol "fn")
+                   (cons (cons ':sb ids) (macro-expand body))))]
+      ;; Expand multi-arity functions
+      ;; (let f ([] (f 10)) ([x] (+ x 10)))
+      ;; (let f (fn [& argv] (match argv [] => (f 10) [x] => (+ x 10))))
+      [(list l (Token a b 'symbol name) bodies ...)
+       (match bodies
+         [(list (list [cons ':sb args] body ...) ...)
+          (define argv : String (symbol->string (gensym 'argv)))
+          (list l (Token a b 'symbol name)
+                (list (Token a b 'symbol "fn")
+                      (list ':sb (Token a b 'symbol "&")
+                            (Token a b 'symbol argv))
+                      (cons (Token a b 'symbol "match")
+                            (cons (Token a b 'symbol argv)
+                                  (expand-multi-arity-let bodies)))))]
+         [_ (cons l (cons (Token a b 'symbol name) bodies))])]
+      [a a]))
+
+  (: expand-multi-arity-let (-> Ast Ast))
+  (define [expand-multi-arity-let stx] 
+    (match stx
+      [(list) (list)]
+      [(cons (list args body ...) xs)
+       (define start-of-body : Token (get-first-token body))
+       (define row (Token-row start-of-body))
+       (define col (Token-col start-of-body))
+       (append (list args (Token row col 'symbol "=>")
+                     (cons (Token row col 'symbol "do") body))
+               (expand-multi-arity-let xs))])) 
+
+  (: get-first-token (-> Ast Token))
+  (define [get-first-token stx]
+    (match stx
+      [(list (Token a b c d) xs ...) (Token a b c d)]
+      [(list (list xs ...) ys ...)
+       (get-first-token xs)]))
+  
+  )
--- tests/frontend/test-expander.rkt
+++ tests/frontend/test-expander.rkt
@@ -1,8 +1,10 @@
 #lang racket/base
 
 (require rackunit)
-(require "../../src/compiler.rkt"
-         "../../src/types.rkt")
+(require
+ "../util.rkt"
+ "../../src/compiler.rkt"
+ "../../src/types.rkt")
 
 (test-equal?
  "Basic parentheses test"
@@ -36,40 +38,74 @@
     ,(Token 1 15 'symbol "x")
     ,(Token 1 17 'integer "2"))))
 
-(test-equal?
- "Nested let a-la Clojure"
- (compile/macro-expand 
-                   "(let [x 10]
+;; Let tests
+(begin
+  (test-equal?
+   "Nested let a-la Clojure"
+   (compile/macro-expand 
+    "(let {x 10}
   (+ x 2))")
- `((,(Token 1 2 'symbol "let")
-    (:sb
-     ,(Token 1 7 'symbol "x")
-     ,(Token 1 9 'integer "10"))
-    (,(Token 2 4 'symbol "+")
-     ,(Token 2 6 'symbol "x")
-     ,(Token 2 8 'integer "2")))))
+   `((,(Token 1 2 'symbol "let")
+      (:cb ,(Token 1 7 'symbol "x")
+           ,(Token 1 9 'integer "10"))
+      (,(Token 2 4 'symbol "+")
+       ,(Token 2 6 'symbol "x")
+       ,(Token 2 8 'integer "2")))))
 
-(test-equal?
- "Defining a fuller function"
- (compile/macro-expand 
-                   "(let f [x y]
+  (test-equal?
+   "Defining a function"
+   (compile/macro-expand 
+    "(let f [x y]
   (let sum (+ x y))
   (println #i\"Sum of {x} and {y} is {sum}\")
   sum)
 
-(f 10 20)")
- `((,(Token 1 2 'symbol "let")
-    ,(Token 1 6 'symbol "f")
-    (:sb ,(Token 1 9 'symbol "x") ,(Token 1 11 'symbol "y"))
-    (,(Token 2 4 'symbol "let")
-     ,(Token 2 8 'symbol "sum")
-     (,(Token 2 13 'symbol "+")
-      ,(Token 2 15 'symbol "x")
-      ,(Token 2 17 'symbol "y")))
-    (,(Token 3 4 'symbol "println")
-     (,(Token 3 12 'reader "#i")
-      ,(Token 3 14 'string "\"Sum of {x} and {y} is {sum}\"")))
-    ,(Token 4 3 'symbol "sum"))
-   (,(Token 6 2 'symbol "f")
-    ,(Token 6 4 'integer "10")
-    ,(Token 6 7 'integer "20"))))
+  (f 10 20)")
+   `((,(Token 1 2 'symbol "let")
+      ,(Token 1 6 'symbol "f")
+      (,(Token 1 6 'symbol "fn")
+       (:sb ,(Token 1 9 'symbol "x") ,(Token 1 11 'symbol "y"))
+       (,(Token 2 4 'symbol "let")
+        ,(Token 2 8 'symbol "sum")
+        (,(Token 2 13 'symbol "+")
+         ,(Token 2 15 'symbol "x")
+         ,(Token 2 17 'symbol "y")))
+       (,(Token 3 4 'symbol "println")
+        (,(Token 3 12 'reader "#i")
+         ,(Token 3 14 'string "\"Sum of {x} and {y} is {sum}\"")))
+       ,(Token 4 3 'symbol "sum")))
+     (,(Token 6 4 'symbol "f")
+      ,(Token 6 6 'integer "10")
+      ,(Token 6 9 'integer "20"))))
+
+  (test-equal?
+   "Define match function"
+   (compile/macro-expand
+    "(let f ([] (f 10)) ([x] (f x 10)) ([x y] (+ x y)))")
+   (let* ([argv (symbol->string (last-gensym 'argv))])
+     `((,(Token 1 2 'symbol "let")
+        ,(Token 1 6 'symbol "f")
+        (,(Token 1 6 'symbol "fn")
+         (:sb ,(Token 1 6 'symbol "&") ,(Token 1 6 'symbol argv))
+         (,(Token 1 6 'symbol "match")
+          ,(Token 1 6 'symbol argv)
+          (:sb)
+          ,(Token 1 13 'symbol "=>")
+          (,(Token 1 13 'symbol "do")
+           (,(Token 1 13 'symbol "f")
+            ,(Token 1 15 'integer "10")))
+          (:sb ,(Token 1 22 'symbol "x"))
+          ,(Token 1 26 'symbol "=>")
+          (,(Token 1 26 'symbol "do")
+           (,(Token 1 26 'symbol "f")
+            ,(Token 1 28 'symbol "x")
+            ,(Token 1 30 'integer "10")))
+          (:sb ,(Token 1 37 'symbol "x")
+               ,(Token 1 39 'symbol "y"))
+          ,(Token 1 43 'symbol "=>")
+          (,(Token 1 43 'symbol "do")
+           (,(Token 1 43 'symbol "+")
+            ,(Token 1 45 'symbol "x")
+            ,(Token 1 47 'symbol "y")))))))))
+  
+  )
